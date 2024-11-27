46d777bbd6a1166f5e2518bb2e970070779348f4
Victor Maia <victorszmaia@gmail.com>
2024-11-27 15:49:35 -0300

    first commit

---
+++ adivinha.py
@@ -0,0 +1,44 @@
+import random
+
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 0
+
+print("qual o nivel de dificuldade")
+print("1 - facil, 2 - medio, 3 - dificil, 4 - brutal, 5 - lucky")
+nivel = int(input("digite o nivel: "))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+    total_de_tentativas = 5
+elif nivel == 4:
+    total_de_tentativas = 3
+elif nivel == 5:
+    total_de_tentativas = 1
+
+for rodada in range(1, total_de_tentativas + 1):
+    print(f"tentativas {rodada} de {total_de_tentativas}")
+    valor_escolhido_str = input("digite um numero entre 1 a 100: ")
+    print("voce digitou ", valor_escolhido_str)
+    valor_escolhido = int(valor_escolhido_str)
+
+    if valor_escolhido < 1 or valor_escolhido > 100:
+        print("numero invalido. o numero deve ser entre 1 a 100")
+        continue
+    acertou = valor_escolhido == numero_secreto
+    maior = valor_escolhido > numero_secreto
+    menor = valor_escolhido < numero_secreto
+
+    if acertou:
+        print(f"you won in {rodada} rounds")
+        print("do you want to play again?")
+        input("Y/N:")
+        break
+    else:
+        if maior :
+            print("muito grande")
+        elif menor :
+            print("muito pequeno")
+
+print("game over")
